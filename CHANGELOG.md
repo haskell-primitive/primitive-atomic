43352d92f7c7bff488d2cfaa5625b71656e6f9ba
Andrew Martin <andrew.thaddeus@gmail.com>
2024-03-26 14:12:13 -0400

    Support primitive-unlifted >= 2.1, drop older primitive-unlifted support

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Revision history for primitive-atomic
 
+## 0.1.0.2 -- 2024-03-26
+
+* Support primitive-unlifted >= 2.1, drop older primitive-unlifted support.
+
 ## 0.1.0.0 -- 2019-04-25
 
 * First version.
--- src/Data/Primitive/Unlifted/Atomic.hs
+++ src/Data/Primitive/Unlifted/Atomic.hs
@@ -8,10 +8,11 @@ module Data.Primitive.Unlifted.Atomic
   ) where
 
 import Control.Monad.Primitive (PrimMonad,PrimState,primitive)
-import Data.Primitive.Unlifted.Array (MutableUnliftedArray(..))
-import Data.Primitive.Unlifted.Class (PrimUnlifted,Unlifted,toUnlifted#,fromUnlifted#)
-import GHC.Exts (Any,MutableArrayArray#,MutableArray#,ArrayArray#,Int(I#))
-import GHC.Exts (casArray#,isTrue#,(==#),unsafeCoerce#)
+import Data.Primitive.Unlifted.Array (MutableUnliftedArray,MutableUnliftedArray_(..))
+import Data.Primitive.Unlifted.Array.Primops (MutableUnliftedArray#(..))
+import Data.Primitive.Unlifted.Class (PrimUnlifted,toUnlifted#,fromUnlifted#)
+import GHC.Exts (Int(I#))
+import GHC.Exts (casArray#,isTrue#,(==#))
 
 -- | Given an array, an offset, the expected old value,
 -- and the new value, perform an atomic compare and swap i.e. write
@@ -29,14 +30,14 @@ casUnliftedArray :: forall m a. (PrimMonad m, PrimUnlifted a)
   -> a -- ^ new value
   -> m (Bool,a)
 {-# INLINE casUnliftedArray #-}
-casUnliftedArray (MutableUnliftedArray arr#) (I# i#) old new =
+casUnliftedArray (MutableUnliftedArray (MutableUnliftedArray# arr#)) (I# i#) old new =
   -- All of this unsafeCoercing is really nasty business. This will go away
   -- once https://github.com/ghc-proposals/ghc-proposals/pull/203 happens.
   -- Also, this is unsound if the result is immidiately consumed by
   -- the FFI.
   primitive $ \s0 ->
-    let !uold = (unsafeCoerce# :: Unlifted a -> Any) (toUnlifted# old)
-        !unew = (unsafeCoerce# :: Unlifted a -> Any) (toUnlifted# new)
-     in case casArray# ((unsafeCoerce# :: MutableArrayArray# (PrimState m) -> MutableArray# (PrimState m) Any) arr#) i# uold unew s0 of
-          (# s1, n, ur #) -> (# s1, (isTrue# (n ==# 0# ),fromUnlifted# ((unsafeCoerce# :: Any -> Unlifted a) ur)) #)
+    let !uold = toUnlifted# old
+        !unew = toUnlifted# new
+     in case casArray# arr# i# uold unew s0 of
+          (# s1, n, ur #) -> (# s1, (isTrue# (n ==# 0# ),fromUnlifted# ur) #)
 
